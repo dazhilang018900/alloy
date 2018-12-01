ad7182b0c5597195245f6d6682fa7ce639c71988
Blake Lucas <blake.c.lucas@gmail.com>
2018-11-30 18:28:05 -0800

    bug fix

--- src/image/CubeLUT.cpp
+++ src/image/CubeLUT.cpp
@@ -1,494 +1,227 @@
-/*
- Copyright (C) 2016 Rocco Meli (rocco.meli@epfl.ch)
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "CubeLUT.h"
-#include <fstream>
-#include <cmath>
-#include <cstdlib>
-#include <iomanip>
-#include <utility>
+#include <iostream>
+#include <sstream>
 namespace aly {
-void ReadCubeFromFile(const std::string& f,CubeLUT& lut){
-	lut.load(f);
-}
-double Gaussian1D(double r, double mu, double sigma) {
-	double norm(1. / (std::sqrt(2. * M_PI) * sigma));
-	return norm * std::exp(-std::pow(r - mu, 2) / (2. * std::pow(sigma, 2)));
-}
-double Gaussian3D(double3 r, double3 mu, double3 sigma) {
-	double g = 1;
-	g *= Gaussian1D(r.x, mu.x, sigma.x);
-	g *= Gaussian1D(r.x, mu.y, sigma.y);
-	g *= Gaussian1D(r.x, mu.z, sigma.z);
-	return g;
-}
-double CubeLUT::evaluate(double ri, double gi, double bi) const {
-	return 0;
-}
-CubeLUT::CubeLUT() :
-		atomCount(0), rows(0), cols(0), slices(0)  {
-}
-CubeLUT::CubeLUT(std::string fname) :
-		atomCount(0), rows(0), cols(0), slices(0) {
-	load(fname);
-}
-
-void CubeLUT::load(std::string fname) {
-	// Open CubeLUT file
-	std::fstream in(fname);
-
+void ReadCubeFromFile(const std::string& f, CubeLUT& lut) {
+	std::ifstream in(f);
 	if (!in.is_open()) {
-		std::cerr << "ERROR: Impossible to open cube file." << std::endl;
-		std::exit(-1);
+		throw std::runtime_error("ERROR: Impossible to open cube file.");
 	}
-
-	// Store first two comment lines
-	std::getline(in, comment1);
-	std::getline(in, comment2);
-
-	// Number of atoms and origin
-	in >> atomCount >> origin[0] >> origin[1] >> origin[2];
-
-	// Voxel properties (number and axis)
-	in >> rows >> xAxis[0] >> xAxis[1] >> xAxis[2];
-	in >> cols >> yAxis[0] >> yAxis[1] >> yAxis[2];
-	in >> slices >> zAxis[0] >> zAxis[1] >> zAxis[2];
-
-	// Preallocate vector for atoms
-	atoms.resize(atomCount);
-
-	// Store atoms
-	for (unsigned int i(0); i < atomCount; i++) {
-		// Store atom
-		in >> atoms[i][0] >> atoms[i][1] >> atoms[i][2] >> atoms[i][3]
-				>> atoms[i][4];
-	}
-
-	// Total number of voxels
-	unsigned long int Nvol(rows * cols * slices);
-
-	// Preallocate vector for volumetric data
-	data.resize(Nvol);
-
-	// Store volumetric data
-	for (unsigned long int i(0); i < Nvol; i++) {
-		in >> data[i];
+	CubeLUT::LUTState status=lut.LoadCubeFile(in);
+	if(status!=CubeLUT::LUTState::OK){
+		throw std::runtime_error(MakeString()<<"Failed to load cube file. Error "<<static_cast<int>(status));
 	}
 }
+float3 CubeLUT::operator()(float x, float y, float z) const {
+	int i = static_cast<int>(std::floor(x));
+	int j = static_cast<int>(std::floor(y));
+	int k = static_cast<int>(std::floor(z));
+	float3 rgb000 = (operator()(i, j, k));
+	float3 rgb100 = (operator()(i + 1, j, k));
+	float3 rgb110 = (operator()(i + 1, j + 1, k));
+	float3 rgb010 = (operator()(i, j + 1, k));
+	float3 rgb001 = (operator()(i, j, k + 1));
+	float3 rgb101 = (operator()(i + 1, j, k + 1));
+	float3 rgb111 = (operator()(i + 1, j + 1, k + 1));
+	float3 rgb011 = (operator()(i, j + 1, k + 1));
+	float dx = x - i;
+	float dy = y - j;
+	float dz = z - k;
+	float3 lower = ((rgb000 * (1.0f - dx) + rgb100 * dx) * (1.0f - dy)
+			+ (rgb010 * (1.0f - dx) + rgb110 * dx) * dy);
+	float3 upper = ((rgb001 * (1.0f - dx) + rgb101 * dx) * (1.0f - dy)
+			+ (rgb011 * (1.0f - dx) + rgb111 * dx) * dy);
+	return (1.0f - dz) * lower + dz * upper;
 
-void CubeLUT::print(std::ostream& out) const {
-	printHeader(out);
-	printAtoms(out);
-	printData(out);
 }
-
-void CubeLUT::printHeader(std::ostream& out) const {
-	// Print first two comment lines
-	out << comment1 << std::endl;
-	out << comment2 << std::endl;
-
-	// Print number of atoms and origin
-	out << std::setw(5) << atomCount;
-	for (unsigned int i(0); i < 3; i++) {
-		out << std::fixed << std::setprecision(6) << std::setw(12) << origin[i];
-	}
-	out << std::endl;
-
-	// Print voxel properties (number and axis): A
-	out << std::setw(5) << rows;
-	for (unsigned int i(0); i < 3; i++) {
-		out << std::fixed << std::setprecision(6) << std::setw(12) << xAxis[i];
-	}
-	out << std::endl;
-
-	// Print voxel properties (number and axis): B
-	out << std::setw(5) << cols;
-	for (unsigned int i(0); i < 3; i++) {
-		out << std::fixed << std::setprecision(6) << std::setw(12) << yAxis[i];
-	}
-	out << std::endl;
-
-	// Print voxel properties (number and axis): C
-	out << std::setw(5) << slices;
-	for (unsigned int i(0); i < 3; i++) {
-		out << std::fixed << std::setprecision(6) << std::setw(12) << zAxis[i];
-	}
-	out << std::endl;
+float3 CubeLUT::operator()(int i, int j, int k) const {
+	return (LUT3D.size() > 0) ? LUT3D[clamp(i,0,rows-1)][clamp(j,0,cols-1)][clamp(k,0,slices-1)] : LUT1D[clamp(i,0,rows-1)];
 }
-
-void CubeLUT::printAtoms(std::ostream& out) const {
-	// Print atoms
-	for (unsigned int i(0); i < atomCount; i++) {
-		out << std::fixed << std::setprecision(0) << std::setw(5)
-				<< atoms[i][0];
-
-		for (unsigned int j(1); j < 5; j++) {
-			out << std::fixed << std::setprecision(6) << std::setw(12)
-					<< atoms[i][j];
+std::string CubeLUT::ReadLine(std::ifstream & infile, char lineSeparator) {
+	// Skip empty lines and comments
+	const char CommentMarker = '#';
+	std::string textLine("");
+	while (textLine.size() == 0 || textLine[0] == CommentMarker) {
+		if (infile.eof()) {
+			status = LUTState::PrematureEndOfFile;
+			break;
 		}
-
-		out << std::endl;
-	}
-}
-
-void CubeLUT::printData(std::ostream& out, int lines) const {
-	// Total number of voxels
-	unsigned long int Nvol(rows * cols * slices);
-
-	// Voxel to print
-	unsigned long int min(0);
-	unsigned long int max(Nvol);
-
-	if (lines < 0) {
-		min = Nvol + lines * 6;
-	} else if (lines > 0) {
-		max = lines * 6;
-	}
-
-	// Print volumetric data
-	for (unsigned int i(min); i < max; i++) {
-		out << std::fixed << std::scientific << std::setprecision(5)
-				<< std::setw(13) << data[i];
-
-		if ((i + 1) % 6 == 0) {
-			out << std::endl;
+		std::getline(infile, textLine, lineSeparator);
+		if (infile.fail()) {
+			status = LUTState::ReadError;
+			break;
 		}
 	}
-}
-
-bool CubeLUT::header_is_compatible(const CubeLUT& cube,
-		double threshold) const {
-	// Compare origin coordinate by coordinate
-	for (unsigned int i(0); i < 3; i++) {
-		if (std::abs(origin[i] - cube.origin[i]) > threshold) {
-			return false;
+	return textLine;
+} // ReadLine
+float3 CubeLUT::ParseTableRow(const std::string & lineOfText) {
+	float3 f;
+	std::istringstream line(lineOfText);
+	for (int i = 0; i < 3; i++) {
+		line >> f[i];
+		if (line.fail()) {
+			status = LUTState::CouldNotParseTableData;
+			break;
+		};
+	}
+	return f;
+} // ParseTableRow
+CubeLUT::LUTState CubeLUT::LoadCubeFile(std::ifstream & infile) {
+	// Set defaults
+	status = LUTState::OK;
+	title.clear();
+	domainMin = float3( 0.0f);
+	domainMax = float3( 1.0f);
+	LUT1D.clear();
+	LUT3D.clear();
+	// Read file data line by line
+	const char NewlineCharacter = '\n';
+	char lineSeparator = NewlineCharacter;
+	// sniff use of legacy lineSeparator
+	const char CarriageReturnCharacter = '\r';
+	for (int i = 0; i < 255; i++) {
+		char inc = infile.get();
+		if (inc == NewlineCharacter)
+			break;
+		if (inc == CarriageReturnCharacter) {
+			if (infile.get() == NewlineCharacter)
+				break;
+			lineSeparator = CarriageReturnCharacter;
+			std::cout
+					<< "INFO: This file uses non-compliant line separator \\r (0x0D)"
+					<< std::endl;
+			break;
+		}
+		if (i > 250) {
+			status = LUTState::LineError;
+			break;
 		}
 	}
+	infile.seekg(0);
+	infile.clear();
+	// read keywords
+	int N, CntTitle, CntSize, CntMin, CntMax;
 
-	// Compare number of voxels in every direction
-	if ((rows != cube.rows) || (cols != cube.cols) || (slices != cube.slices)) {
-		return false;
-	}
-
-	// Compare axis A
-	for (unsigned int i(0); i < 3; i++) {
-		if (std::abs(xAxis[i] - cube.xAxis[i]) > threshold) {
-			return false;
+	// each keyword to occur zero or one time
+	N = CntTitle = CntSize = CntMin = CntMax = 0;
+	while (status == LUTState::OK) {
+		long linePos = infile.tellg();
+		std::string lineOfText = ReadLine(infile, lineSeparator);
+		if (status != LUTState::OK)
+			break;
+		// Parse keywords and parameters
+		std::istringstream line(lineOfText);
+		std::string keyword;
+		line >> keyword;
+		aly::Trim(keyword);
+		if(keyword.size()==0)continue;
+		if ("+" < keyword && keyword < ":") {
+			// lines of table data come after keywords
+			// restore stream pos to re-read line of data
+			infile.seekg(linePos);
+			break;
+		} else if (keyword == "TITLE" && CntTitle++ == 0) {
+			const char QUOTE = '"';
+			char startOfTitle;
+			line >> startOfTitle;
+			if (startOfTitle != QUOTE) {
+				status = LUTState::TitleMissingQuote;
+				break;
+			}
+			std::getline(line, title, QUOTE); // read to "
+		} else if (keyword == "DOMAIN_MIN" && CntMin++ == 0) {
+			line >> domainMin[0] >> domainMin[1] >> domainMin[2];
+		} else if (keyword == "DOMAIN_MAX" && CntMax++ == 0) {
+			line >> domainMax[0] >> domainMax[1] >> domainMax[2];
+		} else if (keyword == "LUT_1D_SIZE" && CntSize++ == 0) {
+			line >> N;
+			if (N < 2 || N > 65536) {
+				status = LUTState::LUTSizeOutOfRange;
+				break;
+			}
+			LUT1D = table1D(N, float3(0.0f));
+		} else if (keyword == "LUT_3D_SIZE" && CntSize++ == 0) {
+			line >> N;
+			if (N < 2 || N > 256) {
+				status = LUTState::LUTSizeOutOfRange;
+				break;
+			}
+			LUT3D = table3D(N, table2D(N, table1D(N, float3(0.0f))));
+		} else {
+			std::cerr<<"Bad Keyword ["<<keyword<<"]"<<std::endl;
+			status = LUTState::UnknownOrRepeatedKeyword;
+			break;
 		}
-	}
-
-	// Compare axis B
-	for (unsigned int i(0); i < 3; i++) {
-		if (std::abs(yAxis[i] - cube.yAxis[i]) > threshold) {
-			return false;
+		if (line.fail()) {
+			status = LUTState::ReadError;
+			break;
 		}
-	}
-
-	// Compare axis C
-	for (unsigned int i(0); i < 3; i++) {
-		if (std::abs(zAxis[i] - cube.zAxis[i]) > threshold) {
-			return false;
+	} // read keywords
+	if (status ==LUTState::OK && CntSize == 0)
+		status = LUTState::LUTSizeOutOfRange;
+	if (status == LUTState::OK
+			&& (domainMin[0] >= domainMax[0] || domainMin[1] >= domainMax[1]
+					|| domainMin[2] >= domainMax[2]))
+		status = LUTState::DomainBoundsReversed;
+	// read lines of table data
+	if (LUT1D.size() > 0) {
+		N = LUT1D.size();
+		rows = N;
+		cols = 0;
+		slices = 0;
+		for (int i = 0; i < N && status == LUTState::OK; i++) {
+			LUT1D[i] = ParseTableRow(ReadLine(infile, lineSeparator));
 		}
-	}
-
-	return true;
-}
-
-bool CubeLUT::header_is_equal(const CubeLUT& cube, double threshold) const {
-	// Compare number of atoms
-	if (atomCount != cube.atomCount) {
-		return false;
-	}
-
-	// Compare origin, number of voxels and axis
-	if (!header_is_compatible(cube, threshold)) {
-		return false;
-	}
-
-	// Compare atoms
-	for (unsigned int i(0); i < atomCount; i++) {
-		for (unsigned int j(0); j < 5; j++) {
-			if (std::abs(atoms[i][j] - cube.atoms[i][j]) > threshold) {
-				return false;
+	} else {
+		N = LUT3D.size();
+		rows = cols = slices = N;
+		// NOTE that r loops fastest
+		for (int b = 0; b < N && status == LUTState::OK; b++) {
+			for (int g = 0; g < N && status == LUTState::OK; g++) {
+				for (int r = 0; r < N && status == LUTState::OK; r++) {
+					LUT3D[r][g][b] = ParseTableRow(
+							ReadLine(infile, lineSeparator));
+				}
 			}
 		}
 	}
-
-	return true;
-}
-
-void CubeLUT::copy_header(const CubeLUT& cube) {
-	// Copy first two comment lines
-	comment1 = cube.comment1;
-	comment2 = cube.comment2;
-
-	// Copy number of atoms and origin
-	atomCount = cube.atomCount;
-	origin = cube.origin;
-
-	// Copy voxel properties
-	rows = cube.rows;
-	cols = cube.cols;
-	slices = cube.slices;
-	xAxis = cube.xAxis;
-	yAxis = cube.yAxis;
-	zAxis = cube.zAxis;
-
-	// Copy atoms
-	atoms = cube.atoms;
-
-	// Total number of voxels
-	unsigned long int Nvol(rows * cols * slices);
-
-	// Resize data and initialize all elements to zero
-	data.clear();
-	data.resize(Nvol, 0.0);
-
-}
-
-CubeLUT CubeLUT::addsub(const CubeLUT& cube, int pm) const {
-	if (std::abs(pm) != 1) {
-		std::cerr << "\nERROR: PM should be +1 or -1.\n" << std::endl;
-
-		std::exit(-1);
-	}
-
-	// Check if the headers of the two cube files (*THIS and CUBE) are compatible
-	if (header_is_compatible(cube)) {
-		// Check if the headers of the two cube files (*THIS and CUBE) are equal
-		if (!header_is_equal(cube)) {
-			// Warn the user that the cube files contain different atoms and/or atomic positions
-			std::cerr
-					<< "\nWARNING: CubeLUT files contains different atoms or atomic positions."
-					<< std::endl;
-			std::cerr
-					<< "         The header of the first CubeLUT will be considered for the sum.\n"
-					<< std::endl;
+	return status;
+} // LoadCubeFile
+CubeLUT::LUTState CubeLUT::SaveCubeFile(std::ofstream & outfile) {
+	if (status != LUTState::OK)
+		return status; // Write only good Cubes
+	// Write keywords
+	const char SPACE = ' ';
+	const char QUOTE = '"';
+	if (title.size() > 0)
+		outfile << "TITLE" << SPACE << QUOTE << title << QUOTE << std::endl;
+	outfile << "# Created by CubeLUT.cpp" << std::endl;
+	outfile << "DOMAIN_MIN" << SPACE << domainMin[0] << SPACE << domainMin[1]
+			<< SPACE << domainMin[2] << std::endl;
+	outfile << "DOMAIN_MAX" << SPACE << domainMax[0] << SPACE << domainMax[1]
+			<< SPACE << domainMax[2] << std::endl;
+	// Write LUT data
+	if (LUT1D.size() > 0) {
+		int N = LUT1D.size();
+		outfile << "LUT_1D_SIZE" << SPACE << N << std::endl;
+		for (int i = 0; i < N && outfile.good(); i++) {
+			outfile << LUT1D[i][0] << SPACE << LUT1D[i][1] << SPACE
+					<< LUT1D[i][2] << std::endl;
 		}
-	} else // CubeLUT file are not compatible and can't be summed (TODO: origin shift)
-	{
-		std::cerr << "\nERROR: CubeLUT files are not compatible.\n"
-				<< std::endl;
-
-		std::exit(-1);
-	}
-
-	// Create empty cube file
-	CubeLUT sumsub;
-
-	// Copy current header to new cube file
-	sumsub.copy_header(*this);
-
-	// Total number of voxels
-	unsigned long int Nvol(rows * cols * slices);
-
-	// Preallocate data of new cube file
-	sumsub.data.resize(Nvol);
-
-	// Sum voxel by voxel
-	for (unsigned long int i(0); i < Nvol; i++) {
-		// Sum (pm=1) or subtract (pm=-1) input data from data
-		sumsub.data[i] = data[i] + pm * cube.data[i]; // PM is +1 (addition) or -1 (subtraction)
-	}
-
-	return sumsub;
-}
-
-CubeLUT CubeLUT::operator+(const CubeLUT& cube) const {
-	// Call addsub as add (pm=+1)
-	return addsub(cube, +1);
-}
-
-CubeLUT CubeLUT::operator-(const CubeLUT& cube) const {
-	// Call addsub as subtract (pm=-1)
-	return addsub(cube, -1);
-}
-
-CubeLUT CubeLUT::operator*(double m) const {
-	// Create empty cube file
-	CubeLUT mult;
-
-	// Copy current header to new cube file
-	mult.copy_header(*this);
-
-	// Total number of voxels
-	unsigned long int Nvol(rows * cols * slices);
-
-	// Preallocate data of new cube file
-	mult.data.resize(Nvol, 0.0);
-
-	// Multiply voxel by voxel
-	for (unsigned long int i(0); i < Nvol; i++) {
-		mult.data[i] = data[i] * m;
-	}
-
-	return mult;
-}
-
-CubeTensor CubeLUT::reshape() const {
-	CubeTensor datat(rows,
-			std::vector<std::vector<double>>(cols,
-					std::vector<double>(slices, 0.0)));
-	// Loop over axis a
-	for (unsigned int i(0); i < rows; i++) {
-		// Loop over axis b
-		for (unsigned int j(0); j < cols; j++) {
-			// Loop over axis c
-			for (unsigned int k(0); k < slices; k++) {
-				// Tranform linear array DATA into rank-3 tensor DATAR
-				datat[i][j][k] = data[i * cols * slices + j * slices + k];
+	} else {
+		int N = LUT3D.size();
+		outfile << "LUT_3D_SIZE" << SPACE << N << std::endl;
+		// NOTE that r loops fastest
+		for (int b = 0; b < N && outfile.good(); b++) {
+			for (int g = 0; g < N && outfile.good(); g++) {
+				for (int r = 0; r < N && outfile.good(); r++) {
+					outfile << LUT3D[r][g][b][0] << SPACE << LUT3D[r][g][b][1]
+							<< SPACE << LUT3D[r][g][b][2] << std::endl;
+				}
 			}
 		}
-	}
-
-	return datat;
-}
-
-std::vector<double> CubeLUT::getData() const {
-	return data;
-}
-
-double3 CubeLUT::getOrigin() const {
-	// Return origin of CubeLUT file
-	return origin;
-}
-
-double3 CubeLUT::getAxisX() const {
-	// Return axis s
-	return xAxis;
-}
-
-double3 CubeLUT::getAxisY() const {
-	// Return axis b
-	return yAxis;
-}
-
-double3 CubeLUT::getAxisZ() const {
-	// Return axis c
-	return zAxis;
-}
-
-long int CubeLUT::getRows() const {
-	// Return number of voxels along a
-	return rows;
-}
-
-long int CubeLUT::getCols() const {
-	// Return number of voxels along b
-	return cols;
-}
-
-long int CubeLUT::getSlices() const {
-	// Return number of voxels along c
-	return slices;
-}
-
-double CubeLUT::getResolutionX() const {
-	// Compute length of axis a
-	return std::sqrt(
-			xAxis[0] * xAxis[0] + xAxis[1] * xAxis[1] + xAxis[2] * xAxis[2]);
-}
-
-double CubeLUT::getResolutionY() const {
-	// Compute length of axis b
-	return std::sqrt(
-			yAxis[0] * yAxis[0] + yAxis[1] * yAxis[1] + yAxis[2] * yAxis[2]);
-}
-
-double CubeLUT::getResolutionZ() const {
-	// Compute length of axis c
-	return std::sqrt(
-			zAxis[0] * zAxis[0] + zAxis[1] * zAxis[1] + zAxis[2] * zAxis[2]);
-}
-
-double CubeLUT::periodicTransform(double d, unsigned int idir) const {
-	if ((idir < 1) or (idir > 3)) {
-		std::cerr << "ERROR: Invalid IDIR." << std::endl;
-		std::cerr << "       IDIR should be 1 (x), 2 (y) or 3 (z)" << std::endl;
-
-		std::exit(-1);
-	}
-
-	// Cell dimension along IDIR
-	double L(0);
-
-	// Voxel dimension along IDIR
-	double dd(0);
-
-	if (idir == 1) {
-		dd = getResolutionX();
-		L = rows * dd;
-	}
-	if (idir == 2) {
-		dd = getResolutionY();
-		L = cols * dd;
-	}
-	if (idir == 3) {
-		dd = getResolutionZ();
-		L = slices * dd;
-	}
-
-	return d - L * static_cast<int>(d / L + 0.5);
-}
-
-std::vector<std::array<double, 5>> CubeLUT::getAtoms() const {
-	return atoms;
-}
-
-std::vector<std::array<double, 5>> CubeLUT::shiftAtoms(double aa, double bb,
-		double cc) {
-	double La(rows * getResolutionX());
-	double Lb(cols * getResolutionY());
-	double Lc(slices * getResolutionZ());
-
-	double da(0);
-	double db(0);
-	double dc(0);
-
-	std::vector<std::array<double, 5>> shifted_atoms(atomCount,
-			std::array<double, 5> { { 0, 0, 0, 0, 0 } });
-
-	for (unsigned int i(0); i < atomCount; i++) {
-		shifted_atoms[i][0] = atoms[i][0];
-		shifted_atoms[i][1] = atoms[i][0];
-
-		shifted_atoms[i][2] = atoms[i][2] + aa * La;
-		shifted_atoms[i][3] = atoms[i][3] + bb * Lb;
-		shifted_atoms[i][4] = atoms[i][4] + cc * Lc;
-
-		da = shifted_atoms[i][2] - origin[0];
-
-		if (da > La) {
-			shifted_atoms[i][2] -= La;
-		}
-
-		db = shifted_atoms[i][3] - origin[1];
-
-		if (db > Lb) {
-			shifted_atoms[i][3] -= Lb;
-		}
-
-		dc = shifted_atoms[i][4] - origin[2];
-
-		if (dc > Lc) {
-			shifted_atoms[i][4] -= Lc;
-		}
-	}
-
-	return shifted_atoms;
-}
+	} // write 3D LUT
+	outfile.flush();
+	return (outfile.good() ? LUTState::OK : LUTState::WriteError);
+} // SaveCubeFile
 }
--- src/image/CubeLUT.h
+++ src/image/CubeLUT.h
@@ -1,122 +1,40 @@
-/*
- Copyright (C) 2016 Rocco Meli (rocco.meli@epfl.ch)
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#ifndef CUBELUT_H
-#define CUBELUT_H
-#include <iostream>
+#ifndef CubeLUT_H
+#define CubeLUT_H
 #include <string>
-#include <array>
 #include <vector>
+#include <fstream>
 #include "math/AlloyMath.h"
-
-namespace aly {
-double Gaussian1D(double r, double mu, double sigma);
-double Gaussian3D(double3 R, double3 mu, double3 sigma);
-
-typedef std::vector<std::vector<std::vector<double>>> CubeTensor;
+namespace aly{
 class CubeLUT {
 public:
-	// Default constructor (use copy_header to compy the header of a CubeLUT and fill data with zeros)
-	CubeLUT();
-	// Create cube file from data
-	CubeLUT(std::string fname);
-
-	// Load CubeLUT fro file
-	void load(std::string filename);
-
-	// Printing routines (whole cube, only header or only data)
-	void print(std::ostream& out) const;
-	void printHeader(std::ostream& out) const;
-	void printAtoms(std::ostream& out) const;
-	void printData(std::ostream& out, int lines = 0) const;
-	double evaluate(double x, double y, double z) const;
-	// COmpare headers
-	bool header_is_compatible(const CubeLUT& cube,
-			double threshold = 1e-12) const;
-	bool header_is_equal(const CubeLUT& cube, double threshold = 1e-12) const;
-	// Copy header (reinitialize data with the correct number of voxels)
-	void copy_header(const CubeLUT& cube);
-	// Sum two cube files
-	CubeLUT operator+(const CubeLUT& cube) const;
-	// Subtract two cube files
-	CubeLUT operator-(const CubeLUT& cube) const;
-	// Multiply cube data by number
-	CubeLUT operator*(double) const;
-
-	// Return reshaped DATA (rank-3 tensor instead of vector)
-	CubeTensor reshape() const;
-
-	// Return data
-	std::vector<double> getData() const;
-
-	// Return CubeLUT file origin
-	double3 getOrigin() const;
-
-	// Return axis
-	double3 getAxisX() const;
-	double3 getAxisY() const;
-	double3 getAxisZ() const;
-
-	// Return number of voxels
-	long int getRows() const;
-	long int getCols() const;
-	long int getSlices() const;
-
-	// Length of the voxel along a, b or c
-	double getResolutionX() const;
-	double getResolutionY() const;
-	double getResolutionZ() const;
-
-	// Periodic boundary conditions
-	double periodicTransform(double d, unsigned int idir) const;
-
-	// Get atoms
-	std::vector<std::array<double, 5>> getAtoms() const;
-
-	// Shift atoms
-	std::vector<std::array<double, 5>> shiftAtoms(double aa, double bb,
-			double cc);
-
+	typedef std::vector<float3> table1D;
+	typedef std::vector<table1D> table2D;
+	typedef std::vector<table2D> table3D;
+	enum class LUTState {
+		OK = 0, NotInitialized = 1,
+		ReadError = 2, WriteError=3, PrematureEndOfFile=4, LineError=5,
+		UnknownOrRepeatedKeyword = 6, TitleMissingQuote=7, DomainBoundsReversed=8,
+		LUTSizeOutOfRange=9, CouldNotParseTableData=10
+	};
+	LUTState status;
+	std::string title;
+	float3 domainMin;
+	float3 domainMax;
+	table1D LUT1D;
+	table3D LUT3D;
+	int rows,cols,slices;
+	CubeLUT(void):rows(0),cols(0),slices(0) { status = LUTState::NotInitialized; };
+	LUTState LoadCubeFile(std::ifstream & infile);
+	LUTState SaveCubeFile(std::ofstream & outfile);
+	float3 operator()(float x, float y, float z) const;
+	float3 operator()(int i, int j, int k) const;
 private:
-	// Sum (PM=+1) or subtract (PM=-1) two cube files
-	CubeLUT addsub(const CubeLUT& cube, int pm) const;
-
-	// Fist two lines of the CubeLUT file (comments)
-	std::string comment1, comment2;
-
-	// Number of atoms
-	unsigned int atomCount;
-
-	// Origin
-	double3 origin;
-
-	// Number of voxels
-	int rows, cols, slices;
-
-	// Axis vectors
-	double3 xAxis, yAxis, zAxis;
-
-	// Atoms
-	std::vector<std::array<double, 5>> atoms;
-
-	// Data
-	std::vector<double> data;
-
+	std::string ReadLine(std::ifstream & infile, char lineSeparator);
+	float3 ParseTableRow(const std::string & lineOfText);
 };
-void ReadCubeFromFile(const std::string& f,CubeLUT& lut);
+void ReadCubeFromFile(const std::string& f, CubeLUT& lut);
+
 }
+
 #endif
+
